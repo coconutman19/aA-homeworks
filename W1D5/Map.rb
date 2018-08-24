ba13dc4fa9d293cdb8c8a5478eb02229c3dea46d
john <johnhuang121@gmail.com>
2018-08-24 02:11:32 -0700

    Set up suggested framework

--- W1D5/Map.rb
+++ W1D5/Map.rb
@@ -1,20 +1,24 @@
 class Map
 
+  def initialize
+    @array = []
+  end
 
+  def set(key, value)
 
+  end
 
+  def get(key)
 
+  end
 
+  def delete(key)
 
+  end
 
+  def show
 
-
-
-
-
-
-
-
+  end
 
 
 end
