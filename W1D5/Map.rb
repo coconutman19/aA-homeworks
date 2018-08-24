04564a545734e1506233f6eb39b26f8be38c18aa
john <johnhuang121@gmail.com>
2018-08-24 02:28:08 -0700

    Define and coded setter method for map similar to hashes

--- W1D5/Map.rb
+++ W1D5/Map.rb
@@ -1,11 +1,21 @@
 class Map
+  attr_reader :array
 
   def initialize
     @array = []
   end
 
-  def set(key, value)
+  def set(key, value) #like hashes
+    exist_location = nil
+    @array.each_with_index { |el, idx| exist_location = idx if el.first == key}
 
+    if exist_location
+      @array[exist_location].last = value
+    else
+      @array.push([key, value])
+    end
+
+    value
   end
 
   def get(key)
