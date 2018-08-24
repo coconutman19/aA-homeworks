2a7a55795dbadcb9452bb36ea33271edea9bbd7e
john <johnhuang121@gmail.com>
2018-08-24 02:04:42 -0700

    Finish set up of Queue class instance variable and methods

--- W1D5/Queue.rb
+++ W1D5/Queue.rb
@@ -1,19 +1,21 @@
 class Queue
+  attr_reader :array
 
   def initialize
-
+    @array = []
   end
 
   def enqueue(el)
-
+    @array.push(el)
+    el
   end
 
   def dequeue
-
+    @array.shift
   end
 
   def peek
-
+    @array[0]
   end
 
 end
