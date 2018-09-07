82809abaec0b0cd532746b493f8fb13d51a947b4
john <johnhuang121@gmail.com>
2018-09-06 21:17:58 -0700

    Complete meta_corgis homework

--- W3D5/metacorgis/meta_corgis.rb
+++ W3D5/metacorgis/meta_corgis.rb
@@ -110,14 +110,20 @@ class MetaCorgiSnacks
   end
 
   def method_missing(name, *args)
-    info = @snack_box.send("get_#{name}_info(@box_id)")
-    tastiness = @snack_box.send("get_#{name}_tastiness(@box_id)")
+    info = @snack_box.send("get_#{name}_info", @box_id)
+    tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+    name = name.to_s
     result = "#{name}: #{info}: #{tastiness} "
     tastiness > 30 ? "* #{result}" : result
   end
 
-
-  # def self.define_snack(name)
-
-  # end
+  def self.define_snack(name)
+    define_method(name) do
+      info = @snack_box.send("get_#{name}_info", @box_id)
+      tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+      display_name = "#{name.split('_').map(&:capitalize).join(' ')}"
+      result = "#{display_name}: #{info}: #{tastiness}"
+      tastiness > 30 ? "* #{result}" : result
+    end
+  end
 end
